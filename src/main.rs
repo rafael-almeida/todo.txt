89e09df83718018ec8f2d5e7c430da97f7eebc1f
rafael-almeida <rafael.almeida@example.com>
2023-12-26 00:52:02 -0500

    define task struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fmt;
+
+struct Task {
+    id: String,
+    title: String,
+    completed: bool,
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Task {{id: {}, title: \"{}\", completed: {}}}",
+            self.id, self.title, self.completed
+        )
+    }
+}
+
 fn main() {
+    let task = Task {
+        id: "1".to_string(),
+        title: "First Task".to_string(),
+        completed: false,
+    };
+
     println!("Hello, world!");
+    println!("{}", task);
 }
