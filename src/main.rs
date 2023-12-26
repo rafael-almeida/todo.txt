2aaaa9b4a6afbafae308556e6d2bc42d05b8d772
rafael-almeida <rafael.almeida@example.com>
2023-12-26 18:12:59 -0500

    save task to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 #[derive(Debug)]
 struct Task {
     id: isize,
@@ -5,22 +8,32 @@ struct Task {
     completed: bool,
 }
 
-fn add_task(tasks: &mut Vec<Task>, id: &mut isize, title: String) {
+fn add_task(file: &mut File, tasks: &mut Vec<Task>, id: &mut isize, title: String) -> std::io::Result<()>{
     let task = Task {
         id: *id,
-        title: format!("{title} {}", id.to_string()),
+        title: format!("{} {}", title, id),
         completed: false,
     };
 
     *id += 1;
+
+    let file_row = format!("{:?}\n", task);
+    file.write_all(file_row.as_bytes())?;
+
     tasks.push(task);
+
+    Ok(())
 }
 
 fn main() {
     let mut index: isize = 0;
     let mut tasks: Vec<Task> = Vec::new();
-    add_task(&mut tasks, &mut index, "Task".to_string());
-    add_task(&mut tasks, &mut index, "Task".to_string());
-    add_task(&mut tasks, &mut index, "Task".to_string());
+
+    let mut file = File::create("tasks.txt").unwrap();
+
+    add_task(&mut file, &mut tasks, &mut index, "Task".to_string()).unwrap();
+    add_task(&mut file, &mut tasks, &mut index, "Task".to_string()).unwrap();
+    add_task(&mut file, &mut tasks, &mut index, "Task".to_string()).unwrap();
+
     println!("{:#?}", tasks);
 }
